f3550ddc8d4eb9c5ebf2d961cd8e838628a6cb3c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-01-31 11:46:48 -0500

    Add the starting version of the test_compute_tf_cookbook.py.

--- tests/test_compute_tf_cookbook.py
+++ tests/test_compute_tf_cookbook.py
@@ -9,31 +9,3 @@ def test_read_file_populates_data_0():
     assert len(compute_tf_cookbook.data) == 0
     compute_tf_cookbook.read_file("inputs/input.txt")
     assert len(compute_tf_cookbook.data) != 0
-
-
-def test_file_contains_singleline_comment_count_1():
-    """Checks that the singleline comment count works."""
-    # pylint: disable=len-as-condition
-    compute_tf_cookbook.read_file("inputs/input.txt")
-    assert len(compute_tf_cookbook.data) != 0
-
-
-def test_file_contains_singleline_comment_count_2():
-    """Checks that the singleline comment count works."""
-    # pylint: disable=len-as-condition
-    compute_tf_cookbook.read_file("inputs/input.txt")
-    assert len(compute_tf_cookbook.data) != 0
-
-
-def test_file_contains_singleline_comment_count_3():
-    """Checks that the singleline comment count works."""
-    # pylint: disable=len-as-condition
-    compute_tf_cookbook.read_file("inputs/input.txt")
-    assert len(compute_tf_cookbook.data) != 0
-
-
-def test_file_contains_singleline_comment_count_4():
-    """Checks that the singleline comment count works."""
-    # pylint: disable=len-as-condition
-    compute_tf_cookbook.read_file("inputs/input.txt")
-    assert len(compute_tf_cookbook.data) != 0
